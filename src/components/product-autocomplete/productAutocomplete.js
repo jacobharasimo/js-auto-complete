305e298af639b6e3260a5e03bafb1bdb1b4f96d7
Jacob Harasimo <jacobharasimo@gmail.com>
2017-07-23 16:26:23 -0400

    wiring onSubmit event for testing

--- src/components/product-autocomplete/productAutocomplete.js
+++ src/components/product-autocomplete/productAutocomplete.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars,consistent-this */
 
 import * as template from './productAutocomplete.html';
 // custom loaders to parse scss -> css -> string
@@ -7,19 +7,28 @@ const style = require('!to-string-loader!css-loader!sass-loader!./productAutocom
 export class ProductAutocomplete extends HTMLElement {
   // Fires when an instance of the element is created.
   createdCallback() {
-    const element = this.attachShadow({mode: 'closed'});
-    element.innerHTML = template.default;
-    element.innerHTML += `<style>${style}</style>`;
+    this.element = this.attachShadow({mode: 'closed'});
+    this.element.innerHTML = template.default;
+    this.element.innerHTML += `<style>${style}</style>`;
   }
 
   // Fires when an instance was inserted into the document.
   attachedCallback() { /**/
+    this.element.getElementById('productSearch').onsubmit = this.onSubmit;
+    this.element.query('.search_query').onchange = this.onChange;
   }
 
+  onChange(e) {
+    console.log(e);
+  }
   // Fires when an attribute was added, removed, or updated.
   attributeChangedCallback(attrName, oldVal, newVal) {
+    console.log('value change', attrName, 'old:', oldVal, 'new ', newVal);
+  }
 
-    /**/
+  onSubmit(e) {
+    e.preventDefault();
+    console.log('form was submitted');
   }
 }
 
